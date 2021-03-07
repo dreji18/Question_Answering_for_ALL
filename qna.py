3b4871dcdc083627a4b1902434ad6f6876c97843
Deepak John Reji <49631017+dreji18@users.noreply.github.com>
2021-03-07 12:06:42 +0530

    Update qna.py

--- qna.py
+++ qna.py
@@ -314,25 +314,17 @@ def main():
     try:
         if len(images)!= 0:
             st.subheader("🎲 Images")  
-            cols = st.beta_columns(2)
-            num = 0
+            #cols = st.beta_columns(2)
+            #num = 0
             for i in images:
                 if i[-4:] == ".ogg":
                     st.markdown("Ha! I found some Audio for you")
                     st.audio(i, format='audio/ogg')
                 else:
-                    if num == 0:
-                        cols[0].image(
-                            i, width=None, # Manually Adjust the width of the image as per requirement,
-                            use_column_width=True, caption=i
-                        )
-                        num = 1
-                    else:
-                        cols[1].image(
-                            i, width=None, # Manually Adjust the width of the image as per requirement,
-                            use_column_width=True, caption=i
-                        )
-                        num = 0
+                    st.image(
+                        i, width=None, # Manually Adjust the width of the image as per requirement,
+                        use_column_width=True, caption=i
+
     except:
         st.write("Sorry! Images unable to load")
                 
